6d0e4bab8b6773813a4c017b4b39e22790980305
DeanLewis <Dean.Lewis@gateshead.ac.uk>
2019-09-11 12:00:08 +0100

    Basic planning for mobile banking complete#

---
+++ src/weektwo/wednesday/mobilebanking/Login.java
@@ -0,0 +1,32 @@
+package weektwo.wednesday.mobilebanking;
+
+public class Login {
+
+    /*
+    store the username entered by the user
+    store the password entered by the user
+    collection of User objects (All users)
+    new scanner object
+     */
+
+    //Login user -->
+    //Ask user for their username
+    //store username entered in username variable
+    //Ask the user for their password
+    //store the password in the password variable
+    //Check the username against each of the --> cont...
+            //User objects until there is a match
+        //if there is a match
+        //Check if the password entered matches the password
+                //tied to the account with the username
+                //openMenu();
+            //prompt - Password is incorrect
+                //loginUser()
+        //prompt - Username is incorrect
+            //loginUser()
+
+
+    //open menu -->
+    //create a new instance of the MainMenu class
+    //call method from MainMenu that displays the menu
+}
---
+++ src/weektwo/wednesday/mobilebanking/User.java
@@ -0,0 +1,13 @@
+package weektwo.wednesday.mobilebanking;
+
+public class User {
+    /*
+    store users Username
+    store users Password
+    store users Name
+    store a collection of the users accounts
+     */
+
+
+
+}
